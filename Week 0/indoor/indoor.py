8553fb95479654aecdeba2648ecce05d63de68cb
Emily Vu <41875226+evu725@users.noreply.github.com>
2024-05-09 18:28:24 -0400

    pset0 upload

---
+++ Week 0/indoor/indoor.py
@@ -0,0 +1,8 @@
+# Ask user for an input
+user = input()
+
+# Convert all the uppercase to lowercase
+user = user.lower()
+
+# Print user output with lowercase
+print(user)
---
+++ Week 0/playback/playback.py
@@ -0,0 +1,5 @@
+# Ask user for input
+user_input = input()
+
+# Print user input, replacing all spaces with ellipses
+print(user_input.replace(" ", "..."))
